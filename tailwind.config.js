b68c6054579c78ebe7392445f81bade2cf322c9a
dev-nahianrafi <iamalnahiyan@gmail.com>
2023-12-29 23:38:01 +0600

    banner end

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,16 @@ tailwind.config = {
         colors: {
           clifford: '#da373d',
           navbg: '#0D0D0D',
+          white: '#FFF',
         },
         maxWidth: {
             'container': '1280px',
         },
         fontFamily: {
             roboto: "Roboto Condensed, sans-serif",
+        },
+        backgroundImage: {
+            'banner': "url('./images/banner.jpg')",
           }
       }
     }
