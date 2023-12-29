bc2b37adf3c73a0a6f7ef95ed5373300ae72fd5b
dev-nahianrafi <iamalnahiyan@gmail.com>
2023-12-29 22:45:07 +0600

    nav done

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          navbg: '#0D0D0D',
+        },
+        maxWidth: {
+            'container': '1280px',
+        },
+        fontFamily: {
+            roboto: "Roboto Condensed, sans-serif",
+          }
+      }
+    }
+  }
