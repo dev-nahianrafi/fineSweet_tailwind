89ae4fca59f81d4e0b8197cb78e5da9bbfe7f65e
dev-nahianrafi <iamalnahiyan@gmail.com>
2023-12-30 16:41:02 +0600

    crunch part done

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ tailwind.config = {
           primary: '#FFD2A4',
           navbg: '#0D0D0D',
           white: '#FFF',
-          itembg: '#FFF5EB', 
+          itembg: '#FFF5EB',
         },
         maxWidth: {
             'container': '1280px',
@@ -15,6 +15,7 @@ tailwind.config = {
         },
         backgroundImage: {
             'banner': "url('./images/banner.jpg')",
+            overlay : 'linear-gradient(40deg, #060606 16.68%, rgba(0, 0, 0, 0.02) 96.44%);'
           }
       }
     }
